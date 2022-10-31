7c3d9f5e1a1717ca81c37e50e621809fc35c7c40
Seth DImmer <sethdimmer1@gmail.com>
2022-10-31 15:39:50 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
-async function main() {
-console.log(fetch("https://jsonplaceholder.typicode.com/users").json);
-}
 
-main();
