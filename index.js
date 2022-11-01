e566905503c6176c213df0bc5f75959dc458596b
Seth DImmer <sethdimmer1@gmail.com>
2022-11-01 03:48:17 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
 
-async function main(){
-    const users = await fetch("https://jsonplaceholder.typicode.com/users")
-    const usersData = users.json();
-    console.log(usersData)
+async function main() {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const usersData =  await users.json();
+    const userListEl = document.querySelector('.user-list')
+    console.log(usersData);
+    userListEl.innerHTML = usersData
+    .map(
+        (user) =>
+                `<div class="user-card">
+                <div class="user-card__container">
+                <h3>${user.name}</h4>
+                    <p><b>Email:</b> email@email.com</p>
+                    <p><b>Phone:</b> 0000000000</p>
+                    <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+                </div>
+            </div>`
+  )
+     .join("")
 }
 
 main();
