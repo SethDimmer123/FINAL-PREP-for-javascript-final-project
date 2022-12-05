24ed874b5ff3583eea06c2cb3edd96868c169294
Seth DImmer <sethdimmer1@gmail.com>
2022-12-05 04:45:39 -0500

    initial commit

--- user.js
+++ user.js
@@ -1,2 +1,13 @@
 // we are rerouting to a new page and remembering the id they clicked
 
+console.log(localStorage.getItem("id"))
+
+// THIS IS THE END OF API: 1
+
+// HOW DO I GET THE POSTS I WANT ON ID 2
+
+// START OF API: 2
+
+async function main() {
+    fetch("https://jsonplaceholder.typicode.com/posts?userId=:id")
+}
