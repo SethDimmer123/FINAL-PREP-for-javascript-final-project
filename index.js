1c6f143f9933fe5f595b6af20687df36ea9fd50f
Seth DImmer <sethdimmer1@gmail.com>
2022-12-05 03:34:41 -0500

    initial commit

--- index.js
+++ index.js
@@ -4,9 +4,27 @@
 async function main() {
     const users = (await fetch("https://jsonplaceholder.typicode.com/users"));
     const usersData = await users.json();
-    console.log(usersData)
+    console.log(
+    usersData.map(user => `<div class="user-card">
+            <div class="user-card__container">
+            <h3>User's Name</h4>
+                <p><b>Email:</b> email@email.com</p>
+                <p><b>Phone:</b> 0000000000</p>
+                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+            </div>
+        </div>`)
+        );
 }
 
 main();
+
+// i now have an array full of objects
+
+// i want an array full of html elements
+
+// use map method to turn array full of objects to html elements
+
+// use .join method to convert it to a string and set the inner html
+
 
 
