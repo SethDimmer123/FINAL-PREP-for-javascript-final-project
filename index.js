576d5aad17a51e0e3fe204a9c64cfef878b8ca63
Seth DImmer <sethdimmer1@gmail.com>
2022-12-05 04:05:28 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+const userListEl = document.querySelector('.user-list');
 
 async function main() {
     const users = (await fetch("https://jsonplaceholder.typicode.com/users"));
     const usersData = await users.json();
-    const userListEl = document.querySelector('.user-list');
     console.log(usersData);
-    userListEl.innerHTML = usersData
-    .map(
-    (user) => `<div class="user-card">
-            <div class="user-card__container">
-            <h3>U</h4>
-                <p><b>Email:</b> email@email.com</p>
-                <p><b>Phone:</b> 0000000000</p>
-                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
-            </div>
-        </div>`
-        ) .join("")
+    userListEl.innerHTML = usersData.map((user) => userHTML(user)) .join("")
 }
 
 main();
+
+
+
+function userHTML(user) {
+    return `<div class="user-card">
+    <div class="user-card__container">
+    <h3>${user.name}</h4>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone}</p>
+        <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
+        ${user.website}
+        </a></p>
+    </div>
+</div>`
+}
 
 // i now have an array full of objects
 
@@ -28,6 +33,8 @@ main();
 // use map method to turn array full of objects to html elements
 
 // use .join method to convert it to a string and set the inner html
+
+
 
 
 
