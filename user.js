3034b0bc1be78c6441f239e1676e9b873872097a
Seth DImmer <sethdimmer1@gmail.com>
2022-12-05 06:08:20 -0500

    initial commit

--- user.js
+++ user.js
@@ -8,12 +8,27 @@
 
 // START OF API: 2
 
+const postListEl = document.querySelector('.post-list') 
+// we are editing the html inside of the post-list container 
+// class that is from user.html
+// postListEl = document.querySelector('.post-list').innerHTML = 
+// we use map method of the html we want to make dynamic inside the array then use the .join method to convert the array to a string
+
 async function main() {
     const id = localStorage.getItem("id")
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
-
     console.log(postsData)
+    postListEl .innerHTML = postsData.map(posts => `
+    <div class="post">
+    <div class="post__title">
+      ${posts.title}
+    </div>
+    <p class="post__body">
+      ${posts.body}
+    </p>
+  </div>
+  `).join("") // join method converts the array from html to a string
 }
 
 main();
